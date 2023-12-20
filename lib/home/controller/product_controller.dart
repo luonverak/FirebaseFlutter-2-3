ed725dbbd1cf584c431ab581d3baab8b2f21f8b0
luonverak <112792376+luonverak@users.noreply.github.com>
2023-12-20 14:57:15 +0700

    code commit

--- lib/home/controller/product_controller.dart
+++ lib/home/controller/product_controller.dart
@@ -1,5 +1,29 @@
+import 'dart:io';
+
 import 'package:get/get.dart';
+import 'package:image_picker/image_picker.dart';
 
 class ProductController extends GetxController {
-  
+  File? file;
+  Future openGallery() async {
+    try {
+      final chooseFile =
+          await ImagePicker().pickImage(source: ImageSource.gallery);
+      file = File(chooseFile!.path);
+    } catch (e) {
+      print(e);
+    }
+    update();
+  }
+
+  Future openCamerea() async {
+    try {
+      final chooseFile =
+          await ImagePicker().pickImage(source: ImageSource.camera);
+      file = File(chooseFile!.path);
+    } catch (e) {
+      print(e);
+    }
+    update();
+  }
 }
--- lib/home/view/add_edit_screen.dart
+++ lib/home/view/add_edit_screen.dart
@@ -1,7 +1,8 @@
-import 'dart:io';
-
+import 'package:adaptive_action_sheet/adaptive_action_sheet.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_firebase/home/controller/product_controller.dart';
 import 'package:flutter_firebase/home/model/prooduct_model.dart';
+import 'package:get/get.dart';
 import '../widget/input_field.dart';
 import '../widget/colors.dart';
 
@@ -10,14 +11,14 @@ class AddAndEditScreen extends StatelessWidget {
   ProductModel? productModel;
   var name = TextEditingController();
   var price = TextEditingController();
-  File? _file;
+  final controller = Get.put(ProductController());
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: appBarColor,
         title: Text(
-          (productModel != null) ? 'Add product' : 'Edit product',
+          (productModel == null) ? 'Add product' : 'Edit product',
         ),
         actions: [
           IconButton(
@@ -26,58 +27,100 @@ class AddAndEditScreen extends StatelessWidget {
           ),
         ],
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: Column(
-          children: [
-            Stack(
-              children: [
-                Container(
-                  width: 200,
-                  height: 200,
-                  decoration: BoxDecoration(
-                      color: Colors.amber,
-                      border: Border.all(width: 3, color: Colors.blue),
-                      borderRadius: BorderRadius.circular(100),
-                      image: _file == null
-                          ? const DecorationImage(
-                              image: AssetImage('asset/image/9212299.jpg'))
-                          : DecorationImage(image: FileImage(_file!))),
-                ),
-                Positioned(
-                  right: 15,
-                  bottom: 18,
-                  child: Container(
-                    decoration: BoxDecoration(
-                      color: const Color.fromARGB(255, 218, 218, 218),
-                      borderRadius: BorderRadius.circular(3),
-                    ),
-                    child: const Padding(
-                      padding: EdgeInsets.all(5.0),
-                      child: Icon(
-                        Icons.camera_alt,
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            children: [
+              Stack(
+                children: [
+                  GetBuilder<ProductController>(
+                    init: ProductController(),
+                    builder: (controller) {
+                      return Container(
+                        width: 200,
+                        height: 200,
+                        decoration: BoxDecoration(
+                          color: Colors.white,
+                          border: Border.all(width: 3, color: Colors.blue),
+                          borderRadius: BorderRadius.circular(100),
+                          image: controller.file == null
+                              ? const DecorationImage(
+                                  image: AssetImage('asset/image/9212299.jpg'),
+                                )
+                              : DecorationImage(
+                                  fit: BoxFit.cover,
+                                  image: FileImage(controller.file!),
+                                ),
+                        ),
+                      );
+                    },
+                  ),
+                  Positioned(
+                    right: 15,
+                    bottom: 18,
+                    child: GestureDetector(
+                      onTap: () {
+                        showAdaptiveActionSheet(
+                          context: context,
+                          androidBorderRadius: 30,
+                          actions: [
+                            BottomSheetAction(
+                              title: const Text('Choose gallery'),
+                              onPressed: (context) async {
+                                await controller
+                                    .openGallery()
+                                    .whenComplete(() => Navigator.pop(context));
+                              },
+                            ),
+                            BottomSheetAction(
+                              title: const Text('Take photo'),
+                              onPressed: (context) async {
+                                await controller
+                                    .openCamerea()
+                                    .whenComplete(() => Navigator.pop(context));
+                              },
+                            ),
+                          ],
+                          cancelAction: CancelAction(
+                            title: const Text('Cancel'),
+                          ),
+                        );
+                      },
+                      child: Container(
+                        decoration: BoxDecoration(
+                          color: const Color.fromARGB(255, 98, 98, 98),
+                          borderRadius: BorderRadius.circular(50),
+                        ),
+                        child: const Padding(
+                          padding: EdgeInsets.all(6.0),
+                          child: Icon(
+                            Icons.camera_alt,
+                            color: Colors.white,
+                          ),
+                        ),
                       ),
                     ),
-                  ),
-                )
-              ],
-            ),
-            const SizedBox(
-              height: 10,
-            ),
-            InputField(
-              controller: name,
-              hintText: 'Enter product name',
-            ),
-            const SizedBox(
-              height: 10,
-            ),
-            InputField(
-              controller: price,
-              hintText: 'Enter product name',
-              maxLine: 10,
-            ),
-          ],
+                  )
+                ],
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              InputField(
+                controller: name,
+                hintText: 'Enter product name',
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              InputField(
+                controller: price,
+                hintText: 'Enter product name',
+                maxLine: 10,
+              ),
+            ],
+          ),
         ),
       ),
     );
