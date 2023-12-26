e532a3341d9dd94864f07fba1aa72bfa230fc20a
luonverak <112792376+luonverak@users.noreply.github.com>
2023-12-26 15:12:05 +0700

    code commit

--- lib/home/widget/loading.dart
+++ lib/home/widget/loading.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:get/get_core/src/get_main.dart';
 
 Future<void> openLoading() async {
   await Future.delayed(Duration.zero,
